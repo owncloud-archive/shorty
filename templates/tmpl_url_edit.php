0e46bb7b900d2ce37521f33459a57e3d04977519
Christian Reiner <github@christian-reiner.info>
2012-04-23 22:25:34 +0000

    shorty: trivial formal fix to markup

--- templates/tmpl_url_edit.php
+++ templates/tmpl_url_edit.php
@@ -72,7 +72,7 @@
     </select>
     <span style="display:inline;">
     <label for="until"><?php echo OC_Shorty_L10n::t('Expiration:'); ?></label>
-    <input id="until" name="until" type="text" maxlength="10" data="" class="" style="width:12em"
+    <input id="until" name="until" type="text" maxlength="10" data="" class="" style="width:12em;"
            placeholder="-<?php echo OC_Shorty_L10n::t('never'); ?>-"
            icon="<?php echo OC_Helper::imagePath('shorty', 'calendar.png'); ?>"/>
     </span>
