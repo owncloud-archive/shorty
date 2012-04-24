cc17a40c3fc82c66ce24fee366b73c80681827e6
Christian Reiner <github@christian-reiner.info>
2012-04-24 20:45:23 +0000

    shorty: german translation of the preferences section

--- l10n/de.php
+++ l10n/de.php
@@ -56,4 +56,64 @@
 "delete shorty"                                              => "Shorty löschen",
 "use shorty"                                                 => "Shorty nutzen",
 "open target url"                                            => "Ziel-Url öffnen",
+"Add as 'shorty' to ownCloud"                                => "Als 'Shorty' zu ownCloud hinzufügen",
+"Drag this to your browser bookmarks."                       => "Zieh' dies in die Lesezeichen Deines Browsers.",
+"Click it, for whatever site you want to create a Shorty."   => "Klicke es, für welche Seite auch immer Du ein 'Shorty' generieren willst.",
+"Backend"                                                    => "Backend",
+"Shortlet"                                                   => "Shortlet",
+"Choose a service…"                                          => "Wähle einen Dienst…",
+"Example"                                                    => "Beispiel",
+"Base url"                                                   => "Basis-Url",
+"Specify a backend base url…"                                => "Lege eine Basis-Url für das Backend fest…",
+"Verification by click"                                      => "Prüfung durch Klicken",
+"bit.ly user"         => "bit.ly-Nutzer",
+"bit.ly user name"    => "bit.ly-Nutzerkennung",
+"bit.ly key"          => "bit.ly-Schlüssel",
+"bit.ly users key"    => "bit.ly-Nutzerschlüssel",
+"API access key"      => "API-Zugriffsschlüssel",
+"Account"             => "Nutzerkonto",
+"API key"             => "API-Schlüssel",
+"Google API key"      => "Google API-Schlüssel",
+"Google API account"  => "Google API-Nutzerkonto",
+"tiny.cc user"        => "tiny.cc-Nutzer",
+"tiny.cc user name"   => "tiny.cc-Nutzerkennung",
+"tiny.cc key"         => "tiny.cc-Schlüssel",
+"tiny.cc user key"    => "tiny.cc-Nutzerschlüssel",
+
+"Use the 'bitly.com' service to register a short url for each generated shorty."
+=> "Den 'bitly.com'-Dienst nutzen, um eine Kurz-Url für jeden generierten Shorty zu registrieren.",
+"The service requires you to authenticate yourself by providing a valid bit.ly user name and an '%s'."
+=> "Dieser Dienst erfordert die Authentifizierung durch eine valide bit.ly-Nutzerkennung und einen '%s'",
+"This means you have to register an '%s' at their site first."
+=> "Das bedeutet, zunächst muss ein '%s' auf deren Seite registriert werden.",
+"Use 'turl' service to register a short url for each generated shorty."
+=> "Den 'turl'-Dienst nutzen, um eine Kurz-Url für jeden generierten Shorty zu registrieren.",
+"Use 'ti.ny' service to register a short url for each generated shorty."
+=> "Den 'ti.ny'-Dienst nutzen, um eine Kurz-Url für jeden generierten Shorty zu registrieren.",
+"Use 'cli.gs' service to register a short url for each generated shorty."
+=> "Den 'cli.gs'-Dienst nutzen, um eine Kurz-Url für jeden generierten Shorty zu registrieren.",
+"Use 'is.gd' service to register a short url for each generated shorty."
+=> "Den 'is.gd'-Dienst nutzen, um eine Kurz-Url für jeden generierten Shorty zu registrieren.",
+"Don't use any backend, simply generate direct links to your ownCloud."
+=> "Kein Backend verwenden, einfache direkt Links zur ownCloud erzeugen.",
+"Such links are most likely longer than those generated when using a backend."
+=> "Solche Links werden sicherlich länger sein, als wenn ein Backend eingesetzt wird.",
+"However this option does not rely on any third party service and keeps your shortys under your control."
+=> "Dafür stützt sich diese Option aber nicht auf fremde Angebote und belässt die Shorties unter eigener Kontrolle.",
+"Static, rule-based backend, generates shorty links relative to a given base url."
+=> "Statisches, regelbasiertes Backend, generiert Shorty-Links relativ zu einer gegebenen Basis-Url.",
+"Since this setup depends on server based configuration rules the base url can only be specified in the 'Admin' section of the configuration."
+=> "Da diese Konfiguration von serverseitigen Konfigurationsregeln abhängt, kann die Basis-Url nur unter 'Verwaltung' in der Konfiguration festgelegt werden.",
+"Have a try with the example link provided, click it, it should result in a confirmation that your setup is working."
+=> "Einfach den verfügbaren Beispiel-Link durch Anklicken testen. Das sollte zu einer Bestätigung führen, dass die Konfiguration funktioniert.",
+"Only use this backend, if you can provide a short base url that is mapped the described way. Your shorties won't work otherwise."
+=> "Dieses Backend kann nur verwendet werden, wenn die Basis-Url in der beschriebenen Art umgesetzt wird. Die Shorties werden andernfalls nicht nutzbar sein.",
+"Use Googles 'goo.gl URL Shorten Service' to register a short url for each generated shorty."
+=> "Googles 'goo.gl'-Dienst nutzen, um eine Kurz-Url für jeden generierten Shorty zu registrieren.",
+"You must provide a valid '%s' to use this service."
+=> "Es muss eine valide '%s' angegeben werden um diesen Dienst nutzen zu können. ",
+"This means you require a 'Google API console account'."
+=> "Das bedeutet, dass ein 'Google API console account' benötigt wird",
+"Register a new '%s' at their pages."
+=> "Einen neuen '%s' auf deren Seite registrieren.",
 );
--- templates/tmpl_preferences.php
+++ templates/tmpl_preferences.php
@@ -39,17 +39,24 @@
     <div id="title" class="title"><strong>Shorty</strong></div>
     <div id="settings">
       <!-- shortlet -->
-      <label for="shortlet" class="aspect"><?php echo $l->t('Shortlet');?></label>
-      <span id="shortlet"><a class="shortlet" href="javascript:(function(){url=encodeURIComponent(location.href);window.open('<?php echo OC_Helper::linkTo('shorty', 'index.php', null, true); ?>?'+url, 'owncloud-shorty')%20})()"><?php echo $l->t('Add shorty to ownCloud'); ?></a></span>
-      <br />
-      <span class="explain"><em><?php echo $l->t('Drag this to your browser bookmarks and click it, whenever you want to create a Shorty.'); ?></em></span>
-      <br />
+      <label for="shortlet" class="aspect"><?php echo $l->t("Shortlet");?></label>
+      <span id="shortlet">
+        <a class="shortlet"
+           href="javascript:(function(){url=encodeURIComponent(location.href);window.open('<?php echo OC_Helper::linkTo('shorty', 'index.php', null, true); ?>?'+url, 'owncloud-shorty')%20})()">
+          <?php echo $l->t("Add as 'shorty' to ownCloud"); ?>
+        </a>
+      </span>
+      <p>
+      <span class="explain"><em><?php echo $l->t("Drag this to your browser bookmarks.");
+                                      echo $l->t("Click it, for whatever site you want to create a Shorty."); ?></em></span>
+      </p>
+      <p>
       <!-- backend selection -->
-      <label for="backend-type" class="aspect"><?php echo $l->t('Backend:');?></label>
+      <label for="backend-type" class="aspect"><?php echo $l->t("Backend").':';?></label>
       <!-- list of available backend types -->
       <span style="margin-right:1em;">
         <select id="backend-type" name="backend-type" style="width:11em;" class="chosen"
-                placeholder="<?php echo $l->t('Choose service...'); ?>" >
+                placeholder="<?php echo $l->t("Choose service…"); ?>" >
           <?php
             foreach ( $_['backend-types'] as $value=>$display )
               echo sprintf ( "        <option value=\"%s\" %s>%s</option>\n", $value, ($value==$_['backend-type']?'selected':''), $display );
@@ -62,60 +69,62 @@
         <br/>
         <label for="backend-example" class="aspect"> </label>
         <span id="backend-example">
-          <label for="example" class="aspect"><?php echo $l->t('Example:');?></label>
+          <label for="example" class="aspect"><?php echo $l->t("Example").':';?></label>
           <span id="example" class="example"><?php echo sprintf('http://%s%s<em>&lt;shorty id&gt;</em>',$_SERVER['SERVER_NAME'],OC_Helper::linkTo('shorty','',false)) ?></span>
         </span>
         <br/>
         <span id="explain" class="explain">
-          <?php echo $l->t('Don\'t use any backend, simply generate direct links to your ownCloud.<br />'.
-                           'Such links are most likely longer than those generated when using a backend.<br />'.
-                           'However this option does not rely on any third party service and keeps your shortys under your control.');?>
+          <?php echo sprintf('%s<br />%s<br />%s',
+                             $l->t("Don't use any backend, simply generate direct links to your ownCloud."),
+                             $l->t("Such links are most likely longer than those generated when using a backend."),
+                             $l->t("However this option does not rely on any third party service and keeps your shortys under your control.") ); ?>
         </span>
       </span>
       <!-- backend -static- -->
       <span id="backend-static" class="backend-supplement" style="display:none;">
-        <label for="backend-static-base" class="aspect"><?php echo $l->t('Base url:');?></label>
+        <label for="backend-static-base" class="aspect"><?php echo $l->t("Base url").':';?></label>
         <input id="backend-static-base" type="text" name="backend-static-base"
                readonly disabled value="<?php echo $_['backend-static-base']; ?>"
-               placeholder="<?php echo $l->t('specify a backend base url...');?>" style="width:25em;">
+               placeholder="<?php echo $l->t("Specify a backend base url…"); ?>" style="width:25em;">
         <br/>
         <label for="backend-example" class="aspect"> </label>
         <span id="backend-example">
-          <label for="example" class="aspect"><?php echo $l->t('Example:');?></label>
-          <a id="example" class="example" title="<?php echo $l->t('Verify by clicking');?>">
+          <label for="example" class="aspect"><?php echo $l->t("Example").':';?></label>
+          <a id="example" class="example" title="<?php echo $l->t("Verification by click");?>">
             <?php echo sprintf('http://%s/<em>&lt;service&gt;</em>/<em>&lt;shorty id&gt;</em>',$_SERVER['SERVER_NAME']) ?>
           </a>
         </span>
         <br/>
         <span id="explain" class="explain">
-          <?php echo $l->t('Static, rule-based backend, generates shorty links relative to a given base url.<br />'.
-                           'Since this setup depends on server based configuration rules the base url can only be specified in the \'Admin\' section of the configuration.<br />'.
-                           'Have a try with the example link provided, click it, it should result in a confirmation that your setup is working.<br />'.
-                           'Only use this backend, if you can provide a short base url that is mapped the described way. Your shorties won\'t work otherwise. ');?>
+          <?php echo sprintf("%s<br />\n%s<br />\n%s<br />\n%s",
+                             $l->t("Static, rule-based backend, generates shorty links relative to a given base url."),
+                             $l->t("Since this setup depends on server based configuration rules the base url can only be specified in the 'Admin' section of the configuration."),
+                             $l->t("Have a try with the example link provided, click it, it should result in a confirmation that your setup is working."),
+                             $l->t("Only use this backend, if you can provide a short base url that is mapped the described way. Your shorties won't work otherwise.") ); ?>
         </span>
       </span>
       <!-- backend bit.ly -->
       <span id="backend-bitly" class="backend-supplement" style="display:none;">
-        <label for="backend-bitly-user" class="aspect"><?php echo $l->t('bit.ly user:');?></label>
+        <label for="backend-bitly-user" class="aspect"><?php echo $l->t("bit.ly user").':';?></label>
         <input id="backend-bitly-user" type="text" name="backend-bitly-user" value="<?php echo $_['backend-bitly-user']; ?>"
-               maxlength="256" placeholder="<?php echo $l->t('bit.ly user name');?>" style="width:10em;">
-        <label for="backend-bitly-key" class="aspect"><?php echo $l->t('bit.ly key:');?></label>
+               maxlength="256" placeholder="<?php echo $l->t("bit.ly user name");?>" style="width:10em;">
+        <label for="backend-bitly-key" class="aspect"><?php echo $l->t("bit.ly key").':';?></label>
         <input id="backend-bitly-key" type="text" name="backend-bitly-key" value="<?php echo $_['backend-bitly-key']; ?>"
-               maxlength="256" placeholder="<?php echo $l->t('bit.ly key');?>" style="width:18em;">
+               maxlength="256" placeholder="<?php echo $l->t("bit.ly users key");?>" style="width:18em;">
         <br/>
         <label for="backend-example" class="aspect"> </label>
         <span id="backend-example">
-          <label for="example" class="aspect"><?php echo $l->t('Example:');?></label>
+          <label for="example" class="aspect"><?php echo $l->t("Example").':';?></label>
           <span id="example" class="example"><?php echo sprintf('http://bitly.com/<em>&lt;key&gt;</em>') ?></span>
         </span>
         <br/>
         <span id="explain" class="explain">
-          <?php echo $l->t(
-            'Use the "bitly.com - shorten, share and track your links" service to register a short url for each generated shorty.<br />'.
-            'The service requires you to authenticate yourself by providing a valid bit.ly user name and an "%s".<br />'.
-            'This means you have to "%s" at their site first.',array(
-              sprintf('<a class="external" href="http://bitly.com/a/your_api_key" target="_blank">%s</a>',$l->t('api access key')),
-              sprintf('<a class="external" href="http://bitly.com/a/sign_up" target="_blank">%s</a>',$l->t('register an account')) ) ); ?>
+          <?php echo sprintf("%s<br />\n%s<br />\n%s",
+                             $l->t("Use the 'bitly.com' service to register a short url for each generated shorty."),
+                             $l->t("The service requires you to authenticate yourself by providing a valid bit.ly user name and an '%s'."),
+                             $l->t("This means you have to '%s' at their site first.", array (
+                                   sprintf('<a class="external" href="http://bitly.com/a/your_api_key" target="_blank">%s</a>',$l->t("API access key")),
+                                   sprintf('<a class="external" href="http://bitly.com/a/sign_up" target="_blank">%s</a>',$l->t("register an account")) ) ) ); ?>
         </span>
       </span>
       <!-- backend cligs -->
@@ -123,12 +132,12 @@
         <br/>
         <label for="backend-example" class="aspect"> </label>
         <span id="backend-example">
-          <label for="example" class="aspect"><?php echo $l->t('Example:');?></label>
+          <label for="example" class="aspect"><?php echo $l->t("Example").':';?></label>
           <span id="example" class="example"><?php echo sprintf('http://cli.gs/<em>&lt;key&gt;</em>') ?></span>
         </span>
         <br/>
         <span id="explain" class="explain">
-          <?php echo $l->t('Use "cli.gs" service to register a short url for each generated shorty.');?>
+          <?php echo $l->t("Use 'cli.gs' service to register a short url for each generated shorty.");?>
         </span>
       </span>
       <!-- backend is.gd -->
@@ -136,56 +145,57 @@
         <br/>
         <label for="backend-example" class="aspect"> </label>
         <span id="backend-example">
-          <label for="example" class="aspect"><?php echo $l->t('Example:');?></label>
+          <label for="example" class="aspect"><?php echo $l->t("Example").':';?></label>
           <span id="example" class="example"><?php echo sprintf('http://is.gd/<em>&lt;key&gt;</em>') ?></span>
         </span>
         <br/>
         <span id="explain" class="explain">
-          <?php echo $l->t('Use "is.gd" service to register a short url for each generated shorty.');?>
+          <?php echo $l->t("Use 'is.gd' service to register a short url for each generated shorty.");?>
         </span>
       </span>
       <!-- backend google -->
       <span id="backend-google" class="backend-supplement" style="display:none;">
-        <label for="backend-google-key" class="aspect"><?php echo $l->t('API key:');?></label>
+        <label for="backend-google-key" class="aspect"><?php echo $l->t("API key").':';?></label>
         <input id="backend-google-key" type="text" name="backend-google-key" value="<?php echo $_['backend-google-key']; ?>"
-               maxlength="256" placeholder="<?php echo $l->t('Google API console key');?>" style="width:24em;">
+               maxlength="256" placeholder="<?php echo $l->t("Google API key");?>" style="width:24em;">
         <br/>
         <label for="backend-example" class="aspect"> </label>
         <span id="backend-example">
-          <label for="example" class="aspect"><?php echo $l->t('Example:');?></label>
+          <label for="example" class="aspect"><?php echo $l->t("Example").':';?></label>
           <span id="example" class="example"><?php echo sprintf('http://goo.gl/<em>&lt;key&gt;</em>') ?></span>
         </span>
         <br/>
         <span id="explain" class="explain">
-          <?php echo $l->t(
-            'Use Googles "goo.gl URL Shorten Service" to register a short url for each generated shorty.<br />'.
-            'You must provide a valid "%s" to use this service. This means you require a "Google API console account".<br />'.
-            'Register a new "%s" at their pages.',array(
-              sprintf('<a class="external" href="https://code.google.com/apis/console/" target="_blank">%s</a>',$l->t('Google API access key')),
-              sprintf('<a class="external" href="https://code.google.com/apis/console/" target="_blank">%s</a>',$l->t('Google API console account'))) );?>
+          <?php echo sprintf("%s<br />\n%s<br />\n%s<br />\n%s",
+                             $l->t("Use Googles 'goo.gl URL Shorten Service' to register a short url for each generated shorty."),
+                             $l->t("You must provide a valid '%s' to use this service.",
+                                   sprintf('<a class="external" href="https://code.google.com/apis/console/" target="_blank">%s</a>',$l->t("Google API key")) ),
+                             $l->t("This means you require a 'Google API console account'."),
+                             $l->t("Register a new '%s' at their pages.", array(
+                                   sprintf('<a class="external" href="https://code.google.com/apis/console/" target="_blank">%s</a>',$l->t("Google API account")) ) ) );?>
         </span>
       </span>
       <!-- backend tinycc -->
       <span id="backend-tinycc" class="backend-supplement" style="display:none;">
-        <label for="backend-tinycc-user" class="aspect"><?php echo $l->t('tiny.cc user:');?></label>
+        <label for="backend-tinycc-user" class="aspect"><?php echo $l->t("tiny.cc user").':'; ?></label>
         <input id="backend-tinycc-user" type="text" name="backend-tinycc-user" value="<?php echo $_['backend-tinycc-user']; ?>"
-               maxlength="256" placeholder="<?php echo $l->t('tiny.cc user name');?>" style="width:10em;">
-        <label for="backend-tinycc-key" class="aspect"><?php echo $l->t('tiny.cc key:');?></label>
+               maxlength="256" placeholder="<?php echo $l->t("tiny.cc user name"); ?>" style="width:10em;">
+        <label for="backend-tinycc-key" class="aspect"><?php echo $l->t("tiny.cc key").':'; ?></label>
         <input id="backend-tinycc-key" type="text" name="backend-tinycc-key" value="<?php echo $_['backend-tinycc-key']; ?>"
-               maxlength="256" placeholder="<?php echo $l->t('tiny.cc key');?>" style="width:19em;">
+               maxlength="256" placeholder="<?php echo $l->t("tiny.cc user key"); ?>" style="width:19em;">
         <br/>
         <label for="backend-example" class="aspect"> </label>
         <span id="backend-example">
-          <label for="example" class="aspect"><?php echo $l->t('Example:');?></label>
+          <label for="example" class="aspect"><?php echo $l->t("Example").':';?></label>
           <span id="example" class="example"><?php echo sprintf('http://tiny.cc/<em>&lt;key&gt;</em>') ?></span>
         </span>
         <br/>
         <span id="explain" class="explain">
-          <?php echo $l->t(
-            'Use the "tiny.cc" service to register a short url for each generated shorty.<br />'.
-            'The service requires you to authenticate yourself by providing a valid tiny.cc user name and an api access key.<br />'.
-            'This means you have to "%s" at their site first.',array(
-              sprintf('<a class="external" href="http://tiny.ccc/" target="_blank">%s</a>',$l->t('register an account')) ) ); ?>
+          <?php echo sprintf ( "%s<br />\n%s<br />\n%s",
+                               $l->t("Use the 'tiny.cc' service to register a short url for each generated shorty."),
+                               $l->t("The service requires you to authenticate yourself by providing a valid tiny.cc user name and an api access key."),
+                               $l->t("This means you have to register an '%s' at their site first.", array (
+                                     sprintf('<a class="external" href="http://tiny.ccc/" target="_blank">%s</a>', $l->t("account") ) ) ) ); ?>
         </span>
       </span>
       <!-- backend tinyURL -->
@@ -193,12 +203,12 @@
         <br/>
         <label for="backend-example" class="aspect"> </label>
         <span id="backend-example">
-          <label for="example" class="aspect"><?php echo $l->t('Example:');?></label>
+          <label for="example" class="aspect"><?php echo $l->t("Example").':'; ?></label>
           <span id="example" class="example"><?php echo sprintf('http://ti.ny/<em>&lt;key&gt;</em>') ?></span>
         </span>
         <br/>
         <span id="explain" class="explain">
-          <?php echo $l->t('Use "ti.ny" service to register a short url for each generated shorty.');?>
+          <?php echo $l->t("Use 'ti.ny' service to register a short url for each generated shorty.");?>
         </span>
       </span>
       <!-- backend turl -->
@@ -206,14 +216,15 @@
         <br/>
         <label for="backend-example" class="aspect"> </label>
         <span id="backend-example">
-          <label for="example" class="aspect"><?php echo $l->t('Example:');?></label>
+          <label for="example" class="aspect"><?php echo $l->t("Example").':'; ?></label>
           <span id="example" class="example"><?php echo sprintf('http://turl.ca/<em>&lt;key&gt;</em>') ?></span>
         </span>
         <br/>
         <span id="explain" class="explain">
-          <?php echo $l->t('Use "turl" service to register a short url for each generated shorty.');?>
+          <?php echo $l->t("Use 'turl' service to register a short url for each generated shorty."); ?>
         </span>
       </span>
+      </p>
     </div>
     <!-- a (usually hidden) dialog used for verification of the correct setup of the 'static' backend -->
     <?php require_once('tmpl_dlg_verify.php'); ?>
