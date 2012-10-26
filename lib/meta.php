cd2655c1e8529b356f86b730561c82aeca0a0ba3
Christian Reiner <github@christian-reiner.info>
2012-10-26 18:26:42 +0000

    shorty: fixed title extraction that failed under some circumstances when validating remote targets urls.

--- lib/meta.php
+++ lib/meta.php
@@ -103,7 +103,7 @@ class OC_Shorty_Meta
 		if ( FALSE!==($page=curl_exec($handle)) )
 		{
 			// try to extract title from page
-			preg_match ( "/<head>.*<title>(.*)<\/title>.*<\/head>/si", $page, $match );
+			preg_match ( "/<head[^>]*>.*<title>(.*)<\/title>.*<\/head>/si", $page, $match );
 			$meta['title']    = htmlspecialchars_decode ( trim($match[1]) );
 			$meta['staticon'] = self::selectIcon ( 'state', TRUE );
 			// final url after a possible redirection
