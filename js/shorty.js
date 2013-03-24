dca673fe145518668088c5028bc99dcfad613c85
Daniel Molkentin <daniel@molkentin.de>
2013-03-24 18:50:08 +0100

    Remove old repo link

--- js/shorty.js
+++ js/shorty.js
@@ -5,7 +5,6 @@
 * @copyright 2011-2013 Christian Reiner <foss@christian-reiner.info>
 * @license GNU Affero General Public license (AGPL)
 * @link information http://apps.owncloud.com/content/show.php/OC.Shorty?content=150401
-* @link repository https://svn.christian-reiner.info/svn/app/oc/shorty
 *
 * This library is free software; you can redistribute it and/or
 * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
