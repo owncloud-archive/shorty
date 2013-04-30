6762fac1a100702710702f0fdcde096d42a64a2a
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-05-01 00:09:17 +0200

    [tx-robot] updated from transifex

--- shorty/l10n/cy_GB.php
+++ shorty/l10n/cy_GB.php
@@ -3,6 +3,8 @@
 "delete" => "dileu",
 "share" => "rhannu",
 "Close" => "Cau",
+"Notes" => "Nodiadau",
+"Nothing here yet" => "Dim byd yma eto",
 "Save" => "Cadw",
 "Settings" => "Gosodiadau",
 "Title" => "Teitl"
