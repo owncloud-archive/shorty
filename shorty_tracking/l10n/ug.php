8a6f58352a5d5870228b75a31399a141374c064a
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-05-12 00:10:15 +0200

    [tx-robot] updated from transifex

---
+++ shorty_tracking/l10n/ug.php
@@ -0,0 +1,11 @@
+<?php $TRANSLATIONS = array(
+"blocked" => "توسۇلغان",
+"load" => "يۈكلە",
+"Address" => "ئادرېس",
+"Click details" => "چېكىلسە تەپسىلاتى",
+"Clicks" => "چېكىشلەر",
+"Host" => "باش ئاپپارات",
+"Time" => "ۋاقىت",
+"Title" => "ماۋزۇ",
+"User" => "ئىشلەتكۈچى"
+);
