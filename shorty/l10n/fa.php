de01740df43c72a79967b817645fea0f234e6444
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-07-10 00:11:29 +0200

    [tx-robot] updated from transifex

--- shorty/l10n/fa.php
+++ shorty/l10n/fa.php
@@ -1,9 +1,11 @@
 <?php $TRANSLATIONS = array(
+"blocked" => "مسدود شده",
 "deleted" => "حذف شده",
 "never" => "هرگز",
 "delete" => "پاک کردن",
 "share" => "به اشتراک گذاشتن",
 "show" => "نمایش",
+"Clicks" => "کلیک ها",
 "Close" => "بستن",
 "Configuration" => "پیکربندی",
 "Notes" => "یادداشت ها",
--- shorty_tracking/l10n/fa.php
+++ shorty_tracking/l10n/fa.php
@@ -1,6 +1,15 @@
 <?php $TRANSLATIONS = array(
+"Shorty Tracking" => "ردیابی کوتاه",
+"denied" => "منع شد",
+"blocked" => "مسدود شده",
+"load" => "بارگذاری",
 "Address" => "آدرس",
+"Click details" => "کلیک بر روی جزئیات",
+"Clicks" => "کلیک ها",
 "Host" => "میزبانی",
+"List of tracked clicks" => "لیست کلیک های ردیابی شده",
 "Result" => "نتیجه",
-"Title" => "عنوان"
+"Time" => "زمان",
+"Title" => "عنوان",
+"User" => "کاربر"
 );
