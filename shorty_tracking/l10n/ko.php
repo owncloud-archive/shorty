6e320fdb57fe4febed3e8008b0774677b6247d24
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-09-30 06:59:56 -0400

    [tx-robot] updated from transifex

--- shorty_tracking/l10n/ko.php
+++ shorty_tracking/l10n/ko.php
@@ -1,8 +1,17 @@
 <?php $TRANSLATIONS = array(
+"Shorty Tracking" => "단축 추적하기",
+"granted" => "허가됨",
+"denied" => "거부됨",
+"blocked" => "차단됨",
+"load" => "불러오기",
 "Address" => "주소",
+"Click details" => "클릭 세부사항",
 "Clicks" => "클릭",
 "Host" => "호스트",
+"List clicks" => "클릭 목록",
+"List of tracked clicks" => "추적된 클릭들 목록",
 "Result" => "결과",
+"Time" => "시간",
 "Title" => "제목",
 "User" => "사용자 이름"
 );
