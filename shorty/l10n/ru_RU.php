dba9de849a2cf4bd196d0650df9f9bcd00222c1b
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-10-19 08:16:10 -0400

    [tx-robot] updated from transifex

--- shorty/l10n/ru_RU.php
+++ shorty/l10n/ru_RU.php
@@ -1,14 +1,3 @@
 <?php $TRANSLATIONS = array(
-"deleted" => "удалено",
-"never" => "никогда",
-"delete" => "удалить",
-"share" => "сделать общим",
-"show" => "показать",
-"Backend" => "Бэкэнд",
-"Close" => "Закрыть",
-"Configuration" => "Конфигурация",
-"Notes" => "Заметки",
-"Save" => "Сохранить",
-"Settings" => "Настройки",
-"Title" => "Название"
+"Settings" => "Настройки"
 );
