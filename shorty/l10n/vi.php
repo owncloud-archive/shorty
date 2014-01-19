b3b2d039ec3894185ca0956ac5520076efb7250b
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-01-19 00:08:57 -0500

    [tx-robot] updated from transifex

--- shorty/l10n/vi.php
+++ shorty/l10n/vi.php
@@ -7,8 +7,11 @@
 "Backend" => "phụ trợ",
 "Close" => "Đóng",
 "Configuration" => "Cấu hình",
+"Feedback" => "Phản hồi",
 "Notes" => "Ghi chép",
+"Reload" => "Tải ",
 "Save" => "Lưu",
 "Settings" => "Cài đặt",
+"That image can be used when writing documents or setting up web sites" => "Hình ảnh có thể được dùng khi viết tài liệu hoặc thiết lập các webs",
 "Title" => "Tiêu đề"
 );
