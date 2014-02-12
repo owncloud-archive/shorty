c90a8fc6a5501bde8b5516fb77b0285f6a953e92
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-02-12 00:16:02 -0500

    [tx-robot] updated from transifex

--- shorty/l10n/bg_BG.php
+++ shorty/l10n/bg_BG.php
@@ -1,6 +1,8 @@
 <?php $TRANSLATIONS = array(
 "deleted" => "изтрито",
 "never" => "никога",
+"delete" => "Изтриване",
+"share" => "Споделяне",
 "show" => "показва",
 "Backend" => "Администрация",
 "Close" => "Затвори",
