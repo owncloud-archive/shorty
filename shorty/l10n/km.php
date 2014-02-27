7b2d29db3ddd9de285cca3398b5ff3746f005f6e
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-02-27 00:09:11 -0500

    [tx-robot] updated from transifex

--- shorty/l10n/km.php
+++ shorty/l10n/km.php
@@ -1,6 +1,17 @@
 <?php $TRANSLATIONS = array(
+"all" => "ទាំងអស់",
+"blocked" => "បាន​ទប់​ស្កាត់",
+"deleted" => "បាន​លុប",
+"delete" => "លុប",
+"share" => "ចែក​រំលែក",
+"Clicks" => "ការ​ចុច",
 "Close" => "បិទ",
+"Notes" => "កំណត់​ចំណាំ",
+"Preference '%s' retrieved" => "បាន​ទទួល​ចំណូល​ចិត្ត '%s'",
+"Preference '%s' saved" => "បាន​រក្សា​ទុក​ចំណូល​ចិត្ត '%s'",
 "Save" => "រក្សាទុក",
+"Setting '%s' saved" => "បាន​រក្សា​ទុក​ការ​កំណត់ '%s'",
 "Settings" => "ការកំណត់",
-"Title" => "ចំណងជើង"
+"Title" => "ចំណងជើង",
+"Url shortened to: %s" => "បាន​បង្រួម Url ទៅ៖ %s"
 );
--- shorty_tracking/l10n/km.php
+++ shorty_tracking/l10n/km.php
@@ -1,4 +1,16 @@
 <?php $TRANSLATIONS = array(
+"Shorty Tracking" => "ការ​ឃ្លាំ​មើល​ខ្លីៗ",
+"granted" => "ទទួល​បាន",
+"denied" => "បាន​បដិសេធ",
+"blocked" => "បាន​ទប់​ស្កាត់",
+"load" => "ផ្ទុក",
 "Address" => "អាសយដ្ឋាន",
-"Title" => "ចំណងជើង"
+"Click details" => "លម្អិត​នៃ​ការ​ចុច",
+"Clicks" => "ការ​ចុច",
+"Host" => "ម៉ាស៊ីន​ផ្ទុក",
+"List of tracked clicks" => "បញ្ជី​ការ​ចុច​ដែល​បាន​តាមដាន",
+"Result" => "លទ្ធផល",
+"Time" => "ម៉ោង",
+"Title" => "ចំណងជើង",
+"User" => "អ្នកប្រើ"
 );
