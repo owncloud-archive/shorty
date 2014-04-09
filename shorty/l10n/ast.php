143f8144851a26814d911f1822ed9b22c50c5aba
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-04-09 00:09:09 -0400

    [tx-robot] updated from transifex

--- shorty/l10n/ast.php
+++ shorty/l10n/ast.php
@@ -1,5 +1,12 @@
 <?php $TRANSLATIONS = array(
 "never" => "enxamás",
+"delete" => "desaniciar",
+"share" => "compartir",
+"Close" => "Zarrar",
+"Loading" => "Cargando",
 "Notes" => "Notes",
+"Nothing here yet" => "Entá nun hai nada equí",
+"Save" => "Guardar",
+"Settings" => "Axustes",
 "Title" => "Títulu"
 );
