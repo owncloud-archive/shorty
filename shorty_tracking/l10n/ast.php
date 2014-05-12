e36ec0ef7f5b5efa1ec44900ee9df5b54b64ff91
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-05-12 00:09:22 -0400

    [tx-robot] updated from transifex

--- shorty_tracking/l10n/ast.php
+++ shorty_tracking/l10n/ast.php
@@ -1,12 +1,18 @@
 <?php $TRANSLATIONS = array(
+"Shorty Tracking" => "Siguimientu de Shorty",
+"Detailed tracking of all requests to existing Shortys along with an integrated visualization of the access history." => "Siguimientu detalláu de toles solicitúes a Shortys esistentes xunto con una visualización integrada del históricu d'accesos.",
 "granted" => "concedíu",
 "denied" => "refugáu",
 "blocked" => "bloquiáu",
+"load" => "cargar",
 "Address" => "Direición",
 "Click details" => "detalles",
 "Clicks" => "Clics",
 "Host" => "Equipu",
 "List clicks" => "Llistar clicks",
+"List of tracked clicks" => "Llista de clicks grabaos ",
 "Result" => "Resultáu",
-"Title" => "Títulu"
+"Time" => "Hora",
+"Title" => "Títulu",
+"User" => "Usuariu"
 );
