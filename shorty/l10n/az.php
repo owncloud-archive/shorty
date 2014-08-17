1dee2c3928bbca9ee5cf9244d87f333abb3e5f0e
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-17 00:08:46 -0400

    [tx-robot] updated from transifex

---
+++ shorty/l10n/az.php
@@ -0,0 +1,13 @@
+<?php $TRANSLATIONS = array(
+"Plugins" => "Program artırmaları",
+"all" => "hər şey",
+"blocked" => "bloklanıb",
+"deleted" => "silinib",
+"disabled" => "söndürülüb",
+"enabled" => "işə salınıb",
+"Close" => "Bağla",
+"Notes" => "Qeydlər",
+"Nothing here yet" => "Hələki burda heçnə yoxdur",
+"Save" => "Saxlamaq",
+"Settings" => "Quraşdırmalar"
+);
---
+++ shorty_tracking/l10n/az.php
@@ -0,0 +1,8 @@
+<?php $TRANSLATIONS = array(
+"Shorty Tracking" => "Qısa izləmə",
+"Detailed tracking of all requests to existing Shortys along with an integrated visualization of the access history." => "Daxili yetki tarixinin inteqrasiya edilmiş görünüşündə olan mövcud qısa keçidin bütün müraciətlərinin detallı izlənməsi.",
+"granted" => "yetkiləndi",
+"denied" => "qadağan edilib",
+"blocked" => "bloklanıb",
+"load" => "yüklə"
+);
