ca18fefd4ca1b066785863a65c039608d4af9dc1
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-18 00:08:32 -0400

    [tx-robot] updated from transifex

--- shorty/l10n/bn_IN.php
+++ shorty/l10n/bn_IN.php
@@ -2,6 +2,8 @@
 "blocked" => "অবরুদ্ধ",
 "Clicks" => "ক্লিকস",
 "Close" => "বন্ধ",
+"Notes" => "নোট",
+"Nothing here yet" => "কিছুই এখানে নেই এখনো",
 "Save" => "সেভ",
 "Settings" => "সেটিংস",
 "Title" => "পদবি"
