6ca730d4fc90ad32fa12fb89ebbc5e4c5cfe3874
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-21 00:08:42 -0400

    [tx-robot] updated from transifex

--- shorty/l10n/bn_BD.php
+++ shorty/l10n/bn_BD.php
@@ -6,6 +6,7 @@
 "Backend" => "পশ্চাদপট",
 "Close" => "বন্ধ",
 "Configuration" => "কনফিগারেসন",
+"Feedback" => "মতামত",
 "Reload" => "পূনরায় লোড কর",
 "Save" => "সংরক্ষণ",
 "Settings" => "নিয়ামকসমূহ",
--- shorty_tracking/l10n/bn_BD.php
+++ shorty_tracking/l10n/bn_BD.php
@@ -1,5 +1,6 @@
 <?php $TRANSLATIONS = array(
 "Address" => "ঠিকানা",
 "Host" => "হোস্ট",
-"Title" => "শিরোনাম"
+"Title" => "শিরোনাম",
+"User" => "ব্যবহারকারি"
 );
