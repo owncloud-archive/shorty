6b0d37411e0bcd47cb0593b59068d8eb1e52c633
arkascha <github@christian-reiner.info>
2014-10-31 19:44:44 +0100

    Shorty Tracking: Remove further backwards compatibility hacks.

--- shorty_tracking/appinfo/app.php
+++ shorty_tracking/appinfo/app.php
@@ -74,21 +74,4 @@ catch ( Exception $e )
 	return;
 }
 
-// backwards compatibility for OC5's global p() functions
-if (OC_Shorty_Tools::versionCompare('<','4.93')) // OC-5
-{
-	if ( ! function_exists('p'))
-	{
-		function p($string) {
-			print(OCP\Util::sanitizeHTML($string));
-		}
-	}
-	if ( ! function_exists('print_unescaped'))
-	{
-		function print_unescaped($string) {
-			print($string);
-		}
-	}
-}
-
 ?>
