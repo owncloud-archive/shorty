4eff10abee1c774ed35080f86c23c1517170ab37
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-12-27 00:08:13 -0500

    [tx-robot] updated from transifex

--- shorty_tracking/l10n/el.php
+++ shorty_tracking/l10n/el.php
@@ -3,6 +3,7 @@
 "Detailed tracking of all requests to existing Shortys along with an integrated visualization of the access history." => "Αναλυτική παρακολούθηση όλων των αιτημάτων μέσω προυπάρχοντων Shortys μαζί με τη χρήση ενσωμάτωμενης οπτικοποίησης του ιστορικού πρόσβασης,",
 "granted" => "επετράπει",
 "denied" => "απαγορεύθηκε",
+"failed" => "απέτυχε",
 "blocked" => "μπλοκαρίστηκε",
 "load" => "φόρτωση",
 "Address" => "Διεύθυνση",
