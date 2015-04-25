10e57a57a19855db33d1b52e1a89b33fb354fc67
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-04-25 00:08:19 -0400

    [tx-robot] updated from transifex

--- shorty/l10n/sr@latin.js
+++ shorty/l10n/sr@latin.js
@@ -6,6 +6,7 @@ OC.L10N.register(
     "delete" : "brisanje",
     "share" : "deljenje",
     "show" : "prikaži",
+    "error" : "greška",
     "Clicks" : "Klikovi",
     "Close" : "Zatvori",
     "Configuration" : "Podešavanje",
@@ -14,9 +15,11 @@ OC.L10N.register(
     "Loading" : "Učitavanje",
     "Notes" : "Beleške",
     "Nothing here yet" : "Ovde još uvek nema ničega",
+    "Refresh" : "Osveži",
     "Save" : "Snimi",
     "Settings" : "Podešavanja",
     "Title" : "Naslov",
+    "Account" : "Nalog",
     "Location" : "Lokacija"
 },
 "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
--- shorty/l10n/sr@latin.php
+++ shorty/l10n/sr@latin.php
@@ -5,6 +5,7 @@ $TRANSLATIONS = array(
 "delete" => "brisanje",
 "share" => "deljenje",
 "show" => "prikaži",
+"error" => "greška",
 "Clicks" => "Klikovi",
 "Close" => "Zatvori",
 "Configuration" => "Podešavanje",
@@ -13,9 +14,11 @@ $TRANSLATIONS = array(
 "Loading" => "Učitavanje",
 "Notes" => "Beleške",
 "Nothing here yet" => "Ovde još uvek nema ničega",
+"Refresh" => "Osveži",
 "Save" => "Snimi",
 "Settings" => "Podešavanja",
 "Title" => "Naslov",
+"Account" => "Nalog",
 "Location" => "Lokacija"
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);";
