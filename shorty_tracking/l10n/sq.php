543e1eba2ec01a6f24ee9380366716e99987ae44
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-20 00:08:26 -0400

    [tx-robot] updated from transifex

--- shorty_tracking/l10n/sq.php
+++ shorty_tracking/l10n/sq.php
@@ -1,11 +1,21 @@
 <?php
 $TRANSLATIONS = array(
-"blocked" => "Bllokuar",
-"Address" => "Adresa",
+"Shorty Tracking" => "Gjurmim Shorty-sh",
+"Detailed tracking of all requests to existing Shortys along with an integrated visualization of the access history." => "Gjurmim i hollësishëm i krejt kërkesave ndaj Shorty-sh ekzistuese, tok me një vizualizim të integruar të historikut të hyrjeve.",
+"granted" => "e akorduar",
+"denied" => "e mohuar",
+"failed" => "e dështuar",
+"blocked" => "e bllokuar",
+"load" => "ngarko",
+"Address" => "Adresë",
+"Click details" => "Klikoni mbi hollësitë",
 "Clicks" => "Klikime",
-"Host" => "Pritësi",
+"Host" => "Strehë",
+"List clicks" => "Radhit klikimet",
+"List of tracked clicks" => "Listë klikimesh të gjurmuara",
+"Result" => "Përfundim",
 "Time" => "Kohë",
-"Title" => "Titulli",
+"Title" => "Titull",
 "User" => "Përdorues"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
