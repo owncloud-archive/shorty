d180d393e265240d2600ce66844eb9d062949cd6
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-03-05 00:09:27 -0500

    [tx-robot] updated from transifex

--- shorty/l10n/hy.js
+++ shorty/l10n/hy.js
@@ -1,10 +1,12 @@
 OC.L10N.register(
     "shorty",
     {
+    "Plugins" : "Խրվակներ",
     "never" : "երբեք",
     "delete" : "ջնջել",
     "edit" : "խմբագրել",
     "Close" : "Փակել",
+    "Notes" : "Նոթեր",
     "Save" : "Պահպանել",
     "Title" : "Վերնագիր",
     "Account" : "Հաշիվ"
--- shorty/l10n/hy.php
+++ shorty/l10n/hy.php
@@ -1,9 +1,11 @@
 <?php
 $TRANSLATIONS = array(
+"Plugins" => "Խրվակներ",
 "never" => "երբեք",
 "delete" => "ջնջել",
 "edit" => "խմբագրել",
 "Close" => "Փակել",
+"Notes" => "Նոթեր",
 "Save" => "Պահպանել",
 "Title" => "Վերնագիր",
 "Account" => "Հաշիվ"
